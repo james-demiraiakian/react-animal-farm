8369dc2197c26fdead15fd2b06af8f20fe9445d2
James Demiraiakian <james.armen@gmail.com>
2021-11-29 15:35:37 -0800

    not really sure what I'm doing here... With help from TAs I managed to get background showing up

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { animals } from '../../data.js'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return <div className="animal">
+    <img src={`${process.env.PUBLIC_URL}/animals/${}`}
+  </div>
 }
